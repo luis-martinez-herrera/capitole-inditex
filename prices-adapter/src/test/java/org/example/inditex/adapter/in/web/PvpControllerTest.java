44e9e9aa0a547b5f017e1fa685ad662c168493d1
luis martinez <luismartinez@outlook.com.pe>
2023-10-26 17:47:47 -0500

    add app starter

---
+++ prices-adapter/src/test/java/org/example/inditex/adapter/in/web/PvpControllerTest.java
@@ -0,0 +1,88 @@
+package org.example.inditex.adapter.in.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.example.inditex.application.port.in.GetPvpUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+@WebMvcTest(PvpController.class)
+class PvpControllerTest {
+  @Autowired private MockMvc mockMvc;
+
+  @MockBean private GetPvpUseCase getPvpUseCase;
+
+  @DisplayName("Test not found path")
+  @Test
+  void testNotFoundRequest() throws Exception {
+    this.mockMvc.perform(get("/prices")).andDo(print()).andExpect(status().isNotFound());
+  }
+
+  @DisplayName("Test invalid media type")
+  @Test
+  void testBadMediaType() throws Exception {
+    this.mockMvc
+        .perform(get("/prices/pvp"))
+        .andDo(print())
+        .andExpect(status().isUnsupportedMediaType());
+  }
+
+  @DisplayName("Test empty request")
+  @Test
+  void testBadRequest() throws Exception {
+    this.mockMvc
+        .perform(
+            get("/prices/pvp")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Test incomplete request")
+  @Test
+  void testBadRequestWithIncompleteParams() throws Exception {
+    this.mockMvc
+        .perform(
+            get("/prices/pvp")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{\"productId\": 1}"))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Test invalid date format")
+  @Test
+  void testBadDateFormatRequest() throws Exception {
+    this.mockMvc
+        .perform(
+            get("/prices/pvp?productId=1&brandId=1&date=2020-06-14")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Test OK request")
+  @Test
+  void testOkRequest() throws Exception {
+    this.mockMvc
+        .perform(
+            get("/prices/pvp?productId=1&brandId=1&date=" + LocalDateTime.of(2020, 6, 14, 15, 0, 0))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+}
