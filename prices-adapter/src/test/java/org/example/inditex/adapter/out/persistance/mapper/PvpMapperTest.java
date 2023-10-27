4883f221c0e6433856833b422c5584f1f0cf8e72
luis martinez <luismartinez@outlook.com.pe>
2023-10-27 00:31:57 -0500

    add tests

--- prices-adapter/src/test/java/org/example/inditex/adapter/out/persistance/mapper/PvpMapperTest.java
+++ prices-adapter/src/test/java/org/example/inditex/adapter/out/persistance/mapper/PvpMapperTest.java
@@ -2,6 +2,8 @@ package org.example.inditex.adapter.out.persistance.mapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import org.example.inditex.adapter.out.persistance.entity.PvpEntity;
 import org.example.inditex.application.domain.model.Pvp;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,25 @@ class PvpMapperTest {
 
   @Test
   void toPvp() {
+    LocalDateTime start = LocalDateTime.now();
+    LocalDateTime end = start.plusMinutes(1);
+
     PvpEntity entity = new PvpEntity();
-    entity.setBrandId(1);
+    entity.setPriceId(1);
+    entity.setProductId(2);
+    entity.setBrandId(3);
+    entity.setStartDate(start);
+    entity.setEndDate(end);
+    entity.setPrice(BigDecimal.ONE);
+    entity.setCurrency("EUR");
 
     Pvp pvp = PvpMapper.INSTANCE.toPvp(entity);
-    assertEquals(1, pvp.getBrandId());
+    assertEquals(1, pvp.getPriceId());
+    assertEquals(2, pvp.getProductId());
+    assertEquals(3, pvp.getBrandId());
+    assertEquals(start, pvp.getStartDate());
+    assertEquals(end, pvp.getEndDate());
+    assertEquals(BigDecimal.ONE, pvp.getPrice());
+    assertEquals("EUR", pvp.getCurrency());
   }
 }
