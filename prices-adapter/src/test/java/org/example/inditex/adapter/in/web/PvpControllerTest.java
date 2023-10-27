80c5f6d1e026ffa96201c64dcbf176c4ff026cd4
luis martinez <luismartinez@outlook.com.pe>
2023-10-27 01:09:33 -0500

    add 404 handler

--- prices-adapter/src/test/java/org/example/inditex/adapter/in/web/PvpControllerTest.java
+++ prices-adapter/src/test/java/org/example/inditex/adapter/in/web/PvpControllerTest.java
@@ -1,14 +1,16 @@
 package org.example.inditex.adapter.in.web;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
 import org.example.inditex.application.port.in.GetPvpUseCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,7 +26,7 @@ class PvpControllerTest {
   @MockBean
   private GetPvpUseCase getPvpUseCase;
 
-  @DisplayName("Test not found path")
+  @DisplayName("Test path not found")
   @Test
   void testNotFoundRequest() throws Exception {
     this.mockMvc.perform(get("/prices")).andDo(print()).andExpect(status().isNotFound());
@@ -74,6 +76,20 @@ class PvpControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andDo(print())
         .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Test 404")
+  @Test
+  void test404Request() throws Exception {
+    Mockito.when(getPvpUseCase.resolve(Mockito.any())).thenThrow(new NoSuchElementException());
+
+    this.mockMvc
+            .perform(
+                    get("/prices/pvp?productId=1&brandId=1&date=" + LocalDateTime.of(2020, 6, 14, 15, 0, 0))
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isNotFound());
   }
 
   @DisplayName("Test OK request")
